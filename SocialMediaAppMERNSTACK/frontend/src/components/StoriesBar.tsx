7ff0c924d606b46c9b1f9bf65679d8c862ce0db5
Lodhi123-ux <ammar.lodhi@manhattandatainc.com>
2025-11-20 21:08:15 +0500

    Frontend in progress

--- SocialMediaAppMERNSTACK/frontend/src/components/StoriesBar.tsx
+++ SocialMediaAppMERNSTACK/frontend/src/components/StoriesBar.tsx
@@ -3,11 +3,12 @@ import { dummyStoriesData, type StoriesDataType } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModal from "./StoryModal";
+import StoryViewer from "./StoryViewer";
 
 const StoriesBar = () => {
   const [stories, setStories] = useState<StoriesDataType[]>([]);
   const [showModal, setShowModal] = useState(false);
-  const [viewStory, setViewStory] = useState(false);
+  const [viewStory, setViewStory] = useState<StoriesDataType | null>(null);
 
   const fetchStories = async () => {
     setStories(dummyStoriesData);
@@ -35,6 +36,7 @@ const StoriesBar = () => {
         {/* Story Cards */}
         {stories.map((story, index) => (
           <div
+            onClick={() => setViewStory(story)}
             key={index}
             className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-linear-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}
           >
@@ -71,6 +73,10 @@ const StoriesBar = () => {
       {/* Add Story Modal */}
       {showModal && (
         <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />
+      )}
+      {/* View Story Modal */}
+      {viewStory && (
+        <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />
       )}
     </div>
   );
